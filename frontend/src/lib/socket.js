0b0dc477cb2e29e920df62e9cfdb7fd5ee613afd
paresh <iparesh18@gmail.com>
2025-11-18 18:38:09 +0530

    io

--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -1,3 +1,5 @@
+import { io } from "socket.io-client"; 
+
 export const socket = io("https://chatgpt-clone-eh05.onrender.com", {
   withCredentials: true,
   transports: ["websocket", "polling"], // WS first → fallback polling
